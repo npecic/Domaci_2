18a685d5a686fd8f1076173cccf750c1aed14626
npecic <npecic.ia@gmail.com>
2023-12-28 08:25:12 -0500

    Initial commit

---
+++ src/com/company/Kalkulator.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Kalkulator {
+
+    double operand1;
+    double operand2;
+
+
+    public double add(){
+        return this.operand1 + this.operand2;
+    }
+    public double sub(){
+        return this.operand1 - this.operand2;
+    }
+    public double mul(){
+        return this.operand1 * this.operand2;
+    }
+    public double div(){
+        return this.operand1 / this.operand2;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Scanner;
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+///**
+// *
+// * Prvi primer
+// */
+//        Kalkulator  kalkulator1 = new Kalkulator();
+//        kalkulator1.operand1 = 10;
+//        kalkulator1.operand2 = 2;
+//
+//        System.out.println(kalkulator1.add());
+//        System.out.println(kalkulator1.sub());
+//        System.out.println(kalkulator1.mul());
+//        System.out.println(kalkulator1.div());
+//    }
+//}
+
+/**
+ * Drugi metod
+ */
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        Kalkulator  kalkulator1 = new Kalkulator();
+//
+//        System.out.println("Unesite prvi broj: ");
+//        kalkulator1.operand1 = Double.parseDouble(scanner.nextLine());
+//
+//        System.out.println("Unesite operator: ");
+//        String operator = scanner.nextLine();
+//
+//        System.out.println("Unesite drugi broj: ");
+//        kalkulator1.operand2 = Double.parseDouble(scanner.nextLine());
+//
+//
+//        if (operator.contains ("+")){
+//            System.out.println(kalkulator1.add());
+//        }
+//        else if (operator.contains("-")){
+//            System.out.println(kalkulator1.sub());
+//        }
+//        else if (operator.contains("*")){
+//            System.out.println(kalkulator1.mul());
+//        }
+//        else if (operator.contains("/")){
+//            System.out.println(kalkulator1.div());
+//        }
+//        else {
+//            System.out.println("Operator nije podrzan!");
+//        }
+//    }
+//}
